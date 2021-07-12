3ed4e33b6deb7249478f090517eecb65797fff71
Nadin Tamer <nadin.tamer@gmail.com>
2021-07-12 10:04:17 -0700

    Set up Parse backend

---
+++ app/src/main/java/com/example/lexis/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.example.lexis;
+
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.parse_application_id))
+                .clientKey(getString(R.string.parse_client_key))
+                .server(getString(R.string.parse_server))
+                .build()
+        );
+    }
+}
