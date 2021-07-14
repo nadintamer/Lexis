56925cc165c5384686b8840f178f07586e3fd422
Nadin Tamer <nadin.tamer@gmail.com>
2021-07-14 09:39:47 -0700

    Convert Word model to ParseObject subclass

--- app/src/main/java/com/example/lexis/ParseApplication.java
+++ app/src/main/java/com/example/lexis/ParseApplication.java
@@ -1,6 +1,8 @@
 package com.example.lexis;
 
+import com.example.lexis.models.Word;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 import android.app.Application;
 
@@ -11,6 +13,7 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        ParseObject.registerSubclass(Word.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.parse_application_id))
                 .clientKey(getString(R.string.parse_client_key))
--- app/src/main/java/com/example/lexis/models/Word.java
+++ app/src/main/java/com/example/lexis/models/Word.java
@@ -1,25 +1,35 @@
 package com.example.lexis.models;
 
-public class Word {
-    String targetLanguage;
-    String english;
-    Boolean isStarred;
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
 
-    public Word(String targetLanguage, String english) {
-        this.targetLanguage = targetLanguage;
-        this.english = english;
-        this.isStarred = false;
-    }
+@ParseClassName("Word")
+public class Word extends ParseObject {
+    public static final String KEY_TARGET_LANGUAGE = "targetLanguage";
+    public static final String KEY_ENGLISH = "english";
+    public static final String KEY_STARRED = "isStarred";
 
     public String getTargetLanguage() {
-        return targetLanguage;
+        return getString(KEY_TARGET_LANGUAGE);
+    }
+
+    public void setTargetLanguage(String word) {
+        put(KEY_TARGET_LANGUAGE, word);
     }
 
     public String getEnglish() {
-        return english;
+        return getString(KEY_ENGLISH);
     }
 
-    public Boolean getStarred() {
-        return isStarred;
+    public void setEnglish(String word) {
+        put(KEY_ENGLISH, word);
+    }
+
+    public Boolean getIsStarred() {
+        return getBoolean(KEY_STARRED);
+    }
+
+    public void setIsStarred(Boolean starred) {
+        put(KEY_STARRED, starred);
     }
 }
