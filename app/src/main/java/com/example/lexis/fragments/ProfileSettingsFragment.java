59b4be853543f0408594d44d4603ce607a32ba25
Nadin Tamer <nadin.tamer@gmail.com>
2021-07-15 10:24:21 -0700

    Switch back to profile info screen after changing target language

--- app/src/main/java/com/example/lexis/fragments/ProfileSettingsFragment.java
+++ app/src/main/java/com/example/lexis/fragments/ProfileSettingsFragment.java
@@ -5,17 +5,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.example.lexis.R;
 import com.example.lexis.databinding.FragmentProfileSettingsBinding;
 import com.example.lexis.utilities.Utils;
-import com.parse.ParseException;
+import com.google.android.material.navigation.NavigationView;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 import org.parceler.Parcels;
 
@@ -24,6 +28,7 @@ import java.util.List;
 
 public class ProfileSettingsFragment extends Fragment {
 
+    private static final String TAG = "ProfileSettingsFragment";
     private static final String ARG_USER = "user";
     private static final List<String> languageCodes = Arrays.asList("fr", "es", "de", "tr");
 
@@ -63,11 +68,24 @@ public class ProfileSettingsFragment extends Fragment {
         binding.btnSave.setOnClickListener(v -> {
             int selectedItemPosition = binding.spinnerLanguage.getSelectedItemPosition();
             user.put("targetLanguage", languageCodes.get(selectedItemPosition));
-            user.saveInBackground(new SaveCallback() {
-                @Override
-                public void done(ParseException e) {
-                    Toast.makeText(getActivity(), "Saved successfully!", Toast.LENGTH_SHORT).show();
+            user.saveInBackground(e -> {
+                if (e != null) {
+                    Log.e(TAG, "Error saving target language", e);
+                    Toast.makeText(getActivity(), "Error saving target language!", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+
+                Toast.makeText(getActivity(), "Saved successfully!", Toast.LENGTH_SHORT).show();
+
+                // switch back to profile info fragment
+                Fragment infoFragment = ProfileInfoFragment.newInstance(user);
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.profileFragmentContainer, infoFragment).commit();
+
+                // set profile item as selected in drawer navigation
+                Menu menuNav = ((NavigationView) getActivity().findViewById(R.id.navView)).getMenu();
+                MenuItem profile = menuNav.getItem(0);
+                profile.setChecked(true);
             });
         });
     }
