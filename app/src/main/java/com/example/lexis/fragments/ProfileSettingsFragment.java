782301a91dc8149f9fb1c1c579ed0ee238c5ce50
Nadin Tamer <nadin.tamer@gmail.com>
2021-07-15 12:16:08 -0700

    User can update e-mail and password from settings screen

--- app/src/main/java/com/example/lexis/fragments/ProfileSettingsFragment.java
+++ app/src/main/java/com/example/lexis/fragments/ProfileSettingsFragment.java
@@ -16,9 +16,11 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.lexis.R;
+import com.example.lexis.activities.SignupActivity;
 import com.example.lexis.databinding.FragmentProfileSettingsBinding;
 import com.example.lexis.utilities.Utils;
 import com.google.android.material.navigation.NavigationView;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 
 import org.parceler.Parcels;
@@ -64,29 +66,73 @@ public class ProfileSettingsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        binding.etEmail.setText(user.getEmail());
         binding.spinnerLanguage.setSelection(languageCodes.indexOf(Utils.getCurrentTargetLanguage()));
-        binding.btnSave.setOnClickListener(v -> {
-            int selectedItemPosition = binding.spinnerLanguage.getSelectedItemPosition();
-            user.put("targetLanguage", languageCodes.get(selectedItemPosition));
-            user.saveInBackground(e -> {
-                if (e != null) {
-                    Log.e(TAG, "Error saving target language", e);
-                    Toast.makeText(getActivity(), "Error saving target language!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+        binding.btnSave.setOnClickListener(v -> updateUserInformation());
+    }
 
-                Toast.makeText(getActivity(), "Saved successfully!", Toast.LENGTH_SHORT).show();
+    private void updateUserInformation() {
+        String email = binding.etEmail.getText().toString();
+        String password = binding.etPassword.getText().toString();
+        String passwordConfirm = binding.etPasswordConfirm.getText().toString();
 
-                // switch back to profile info fragment
-                Fragment infoFragment = ProfileInfoFragment.newInstance(user);
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.profileFragmentContainer, infoFragment).commit();
+        if (email.isEmpty()) {
+            Toast.makeText(getActivity(), "E-mail cannot be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (!password.equals(passwordConfirm)) {
+            Toast.makeText(getActivity(), "Passwords don't match!", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-                // set profile item as selected in drawer navigation
-                Menu menuNav = ((NavigationView) getActivity().findViewById(R.id.navView)).getMenu();
-                MenuItem profile = menuNav.getItem(0);
-                profile.setChecked(true);
-            });
+        int selectedItemPosition = binding.spinnerLanguage.getSelectedItemPosition();
+        user.put("targetLanguage", languageCodes.get(selectedItemPosition));
+        user.setEmail(email);
+        if (!password.isEmpty()) {
+            user.setPassword(password); // only set new password if field is filled out
+        }
+
+        user.saveInBackground(e -> {
+            if (e != null) {
+                showErrorMessage(e);
+                return;
+            }
+
+            Toast.makeText(getActivity(), "Profile updated successfully!", Toast.LENGTH_SHORT).show();
+
+            // switch back to profile info fragment
+            Fragment infoFragment = ProfileInfoFragment.newInstance(user);
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.profileFragmentContainer, infoFragment).commit();
+
+            // set profile item as selected in drawer navigation
+            Menu menuNav = ((NavigationView) getActivity().findViewById(R.id.navView)).getMenu();
+            MenuItem profile = menuNav.getItem(0);
+            profile.setChecked(true);
         });
+    }
+
+    private void showErrorMessage(ParseException e) {
+        String errorMessage;
+        switch (e.getCode()) {
+            case 101:
+                errorMessage = "Invalid username/password!";
+                break;
+            case 200:
+                errorMessage = "Username cannot be empty!";
+                break;
+            case 201:
+                errorMessage = "Password cannot be empty!";
+                break;
+            case 202:
+                errorMessage = "Username is already in use!";
+                break;
+            case 203:
+                errorMessage = "E-mail is already in use!";
+                break;
+            default:
+                errorMessage = "Error with updating information!";
+                break;
+        }
+        Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_SHORT).show();
     }
 }
