a660036dd3ece081d86a3b025b1a4017d4c44b4b
Nadin Tamer <nadin.tamer@gmail.com>
2021-08-03 14:04:15 -0700

    Added custom navigation drawer view

--- app/src/main/java/com/example/lexis/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/lexis/fragments/ProfileFragment.java
@@ -20,6 +20,12 @@ import com.example.lexis.activities.LoginActivity;
 import com.example.lexis.databinding.FragmentProfileBinding;
 import com.example.lexis.utilities.Utils;
 import com.google.android.material.navigation.NavigationView;
+import com.mikepenz.materialdrawer.holder.ImageHolder;
+import com.mikepenz.materialdrawer.holder.StringHolder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.util.MaterialDrawerSliderViewExtensionsKt;
+import com.mikepenz.materialdrawer.widget.AccountHeaderView;
 import com.parse.ParseUser;
 
 import org.parceler.Parcels;
@@ -69,7 +75,8 @@ public class ProfileFragment extends Fragment {
 
         Utils.setLanguageLogo(binding.toolbar.ivLogo);
         binding.toolbar.ibHamburger.setOnClickListener(v -> openDrawerMenu());
-        setupDrawerContent(binding.navView);
+        setUpMaterialDrawer();
+        // setupDrawerContent(binding.navView);
 
         // set profile info fragment as default
         Fragment infoFragment = ProfileInfoFragment.newInstance(user);
@@ -91,7 +98,75 @@ public class ProfileFragment extends Fragment {
     Open drawer navigation view when hamburger icon is clicked.
     */
     private void openDrawerMenu() {
-        binding.drawerLayout.openDrawer(GravityCompat.END);
+        // binding.drawerLayout.openDrawer(GravityCompat.END);
+        binding.drawerLayout.openDrawer(binding.slider);
+    }
+
+    private void setUpMaterialDrawer() {
+        // binding.slider.setStickyHeaderView();
+
+        MaterialDrawerSliderViewExtensionsKt.addItems(binding.slider,
+                new PrimaryDrawerItem()
+                {
+                    {
+                        setName(new StringHolder(R.string.profile_label));
+                        setIcon(new ImageHolder(R.drawable.profile_icon));
+                        setSelectable(true);
+                        setSelected(true);
+                        setIdentifier(0);
+                    }
+                },
+                new PrimaryDrawerItem()
+                {
+                    {
+                        setName(new StringHolder(R.string.settings_label));
+                        setIcon(new ImageHolder(R.drawable.settings_icon));
+                        setSelectable(true);
+                        setIdentifier(1);
+                    }
+                },
+                new PrimaryDrawerItem()
+                {
+                    {
+                        setName(new StringHolder(R.string.log_out_label));
+                        setIcon(new ImageHolder(R.drawable.log_out_icon));
+                        setSelectable(true);
+                        setIdentifier(2);
+                    }
+                });
+
+        binding.slider.setOnDrawerItemClickListener(((view, drawerItem, integer) -> {
+            if (drawerItem != null) {
+                Fragment fragment = null;
+                if (drawerItem.getIdentifier() == 0) {
+                    fragment = ProfileInfoFragment.newInstance(ParseUser.getCurrentUser());
+                } else if (drawerItem.getIdentifier() == 1) {
+                    fragment = ProfileSettingsFragment.newInstance(ParseUser.getCurrentUser());
+                } else if (drawerItem.getIdentifier() == 2) {
+                    ParseUser.logOut();
+                    goLoginActivity();
+                }
+
+                if (fragment != null) {
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.profileFragmentContainer, fragment).commit();
+                }
+            }
+            return false;
+        }));
+
+        new AccountHeaderView(this.getActivity()) {{
+            addProfiles(
+                    new ProfileDrawerItem()
+                    {{
+                        setName(new StringHolder(ParseUser.getCurrentUser().getUsername()));
+                        setDescription(new StringHolder(ParseUser.getCurrentUser().getEmail()));
+                        setIcon(new ImageHolder(R.drawable.logo_animation));
+                    }}
+            );
+            attachToSliderView(binding.slider);
+            setSelectionListEnabledForSingleProfile(false);
+        }};
     }
 
     /*
